7102b8afe6710ff1b03c45e7fbc964904c82ad1d
Raynagah <rodrigo.vargas.rogel99@gmail.com>
2024-06-23 22:39:32 -0400

    Adición_comentarios

--- funcion_listar.py
+++ funcion_listar.py
@@ -1,13 +1,16 @@
+#Importamos las funciones que utilizaremos a lo largo del código.
 import json, time;
-
+#Creamos la función para listar a los trabajadores
 def listar_trabajadores():
     print("Ha elegido listar trabajadores.");
     try:
+        #Abrimos el archivo que contiene a los trabajadores en modo lectura.
         with open('trabajadores.json', 'r', encoding='utf-8') as archivo:
             trabajadores=json.load(archivo);
     except:
         print("No hay trabajadores para listar, intente agregando trabajadores para comenzar.");
     else:
+        #Incorporamos un ciclo For que utilice el archivo de trabajadores como base para el recorrido que debe hacerse.
         for i in range(len(trabajadores)):
             print(f"Trabajador {i+1}");
             trabajador=(trabajadores[i]);

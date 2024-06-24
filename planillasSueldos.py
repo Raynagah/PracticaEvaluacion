ad959f6495d69f5edb6b07723d787e1732096410
Carlos <ca.moil@duocuc.cl>
2024-06-23 22:19:56 -0400

    doy

---
+++ planillasSueldos.py
@@ -0,0 +1,3 @@
+# Cuando el usuario seleccione mostrar planillas sueldo tendremos 2 tipos, uno con filtro y otro general
+# Cada planilla debe mostrar TRABAJADOR, CARGO, SUELDO BRUTO, DESC. SALUD, DESC. AFP, LIQUIDO A PAGAR
+# Se debe crear un archivo con las planillas
